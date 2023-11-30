b75ea436943af10bc154dc51c99640b17e827d67
AP DURGA MAHALAKSHMI <AP DURGA MAHALAKSHMI@LAPTOP-AB3A18MS>
2023-11-30 09:41:33 +0530

    create testsuits

---
+++ Test Suites/Login TestSuits.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login TestSuits</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>bd13215c-f960-40e8-bd8a-93130eca43d1</testSuiteGuid>
+   <testCaseLink>
+      <guid>9dd5789a-6148-4e75-9be7-29b2311ef40b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Testcases</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
